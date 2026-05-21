f6bec90a57a4f1cc094299b322bb93009a0e44e1
MuhammadNasir44 <muhammadnasirpk44@gmail.com>
2026-05-21 12:20:05 +0500

    some backend changes

--- src/models/User.js
+++ src/models/User.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
     },
     phoneNumber: {
         type: String,
-        required: true,
+        default: '',
         trim: true,
     },
     firebaseUid: {
--- src/server.js
+++ src/server.js
@@ -6,15 +6,19 @@ const { initSocket } = require('./socket');
 
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
+
 const httpServer = http.createServer(app);
+
 initSocket(httpServer, app);
 
 mongoose.connect(MONGODB_URI)
     .then(() => {
         console.log('Connected to MongoDB');
-        httpServer.listen(PORT, () => {
+
+        httpServer.listen(PORT, '0.0.0.0', () => {
             console.log(`Server is running on port ${PORT}`);
         });
+
     })
     .catch((err) => {
         console.error('MongoDB connection error:', err);
